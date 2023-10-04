c34d576e3fe70e25b7a31b9d3fcf18269cd50f5f
devDesignMD <82402719+devDesignMD@users.noreply.github.com>
2023-10-04 20:56:50 +0300

    Created 4 programs

--- src/ConsecutiveNumber.java
+++ src/ConsecutiveNumber.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ConsecutiveNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ionica spune numbarul: ");
+        System.out.println("Ionica spune numarul: ");
         int num = scanner.nextInt();
 
-        System.out.println(num-2 + " " + (num-1) +" " + num + " "  + (num+1) + " " + (num+2));
+        System.out.println((num-2) + " " + (num-1) +" " + num + " "  + (num+1) + " " + (num+2));
     }
 }
--- src/FregventareaGradinitei.java
+++ src/FregventareaGradinitei.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class FregventareaGradinitei {
     public static void main(String[] args) {
-//        Scanner scanner =  new Scanner(System.in);
-//        System.out.print("Enter n zile care a frecventat copilul: ");
-//        double n = scanner.nextDouble();
-//
-//        double plataOZi = 65;
-//        double S = plataOZi * n;
-//        System.out.println("Plata pentru " + n + " este de " + S);
-        System.out.println(60 % 7);
+        Scanner scanner =  new Scanner(System.in);
+        System.out.print("Enter n zile care a frecventat copilul: ");
+        double n = scanner.nextDouble();
+
+        double plataOZi = 65.5;
+        double S = plataOZi * n;
+        System.out.println("Plata pentru " + n + " zile " + " este de " + S);
 
 
     }
